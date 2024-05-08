fcb592bf66381fc37e0eabede030196ab5c5adc9
Carsten Kremser <github@carstenkremser.de>
2024-05-08 14:14:52 +0200

    add TimeRecordRepository

--- backend/src/main/java/de/carstenkremser/workhourcapture/backend/repository/TimeRecordRepository.java
+++ backend/src/main/java/de/carstenkremser/workhourcapture/backend/repository/TimeRecordRepository.java
@@ -4,11 +4,7 @@ import de.carstenkremser.workhourcapture.backend.model.TimeRecord;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface TimeRecordRepository extends MongoRepository<TimeRecord, String> {
 
-    List<TimeRecord> getTimeRecordsByUserId(String userId);
 }
